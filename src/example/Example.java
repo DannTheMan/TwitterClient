a6825d14cda853b8b38662de83ff84327f765d66
Daniel Lehman <lehmandr@rose-hulman.edu>
2015-05-01 21:06:12 -0400

    finished metrics

--- src/example/Example.java
+++ src/example/Example.java
@@ -14,11 +14,11 @@
  * limitations under the License.
  */
 
-package example;
+package src.example;
 
-import com.gist.twitter.FilterParameterFetcher;
-import com.gist.twitter.TwitterClient;
-import com.gist.twitter.UndelimitedTwitterStreamProcessor;
+import src.com.gist.twitter.FilterParameterFetcher;
+import src.com.gist.twitter.TwitterClient;
+import src.com.gist.twitter.UndelimitedTwitterStreamProcessor;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.IOException;
